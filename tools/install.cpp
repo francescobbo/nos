8b4f048a1ee58549b46a2bdb17316a4f1482402d
Francesco Boffa <fra.boffa@gmail.com>
2016-08-05 13:59:45 +0200

    Added installer to inject bootsector into the disk image

---
+++ tools/install.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    cout << "Usage: install DISK_IMAGE BOOT_LOADER" << endl;
+    return -1;
+  }
+
+  char code[512];
+  ifstream loader(argv[2]);
+  loader.read(code, 512);
+  loader.close();
+
+  cout << code << endl;
+
+  fstream file(argv[1], ios::in | ios::out | ios::binary);
+  file.seekp(0);
+  file.write(code, 512);
+  file.close();
+
+  return 0;
+}

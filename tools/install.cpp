1cfd0584b8acbb553a078ffaf9cd888a4f868cfb
Francesco Boffa <fra.boffa@gmail.com>
2016-08-06 19:09:23 +0200

    Updated `install` tool to allow multiple files in a Simple FS

--- tools/install.cpp
+++ tools/install.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    cout << "Usage: install DISK_IMAGE BOOT_LOADER" << endl;
+  if (argc < 3) {
+    cout << "Usage: install DISK_IMAGE STAGE1 [file1 [file2 [file3]]]" << endl;
     return -1;
   }
 
@@ -14,10 +14,44 @@ int main(int argc, char *argv[]) {
   loader.read(code, 512);
   loader.close();
 
-  fstream file(argv[1], ios::in | ios::out | ios::binary);
-  file.seekp(0);
-  file.write(code, 512);
-  file.close();
+  // Open the disk image for writing
+  fstream disk(argv[1], ios::in | ios::out | ios::binary);
+
+  // Add the stage1 boot loader
+  disk.seekp(0);
+  disk.write(code, 512);
+
+  /*
+   * Any following file is stored in contiguous sectors, preceded by a single
+   * sector containing the number of sectors that the file is made of.
+   * The Simplest file system ever.
+   */
+  int lastSector = 0;
+  for (int i = 3; i < argc; i++) {
+    ifstream file(argv[i], ios::binary);
+    char sizeSector[512] = {0, };
+
+    int sectorLength = 0;
+    char buffer[512];
+    while (!file.eof()) {
+      memset(buffer, 0, 512);
+      file.read(buffer, 512);
+
+      disk.seekp((lastSector + sectorLength + 2) * 512, ios::beg);
+      disk.write(buffer, 512);
+      sectorLength++;
+    }
+
+    file.close();
+
+    disk.seekp((lastSector + 1) * 512, ios::beg);
+    ((int *)sizeSector)[0] = sectorLength;
+    disk.write(sizeSector, 512);
+
+    lastSector += sectorLength + 1;
+  }
+
+  disk.close();
 
   return 0;
 }
